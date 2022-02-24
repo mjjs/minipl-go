e8a09aa94efd7b5a7cb64b8518d970a6abb728e3
Mikko Siitonen <mikko.siitonen@helsinki.fi>
2022-02-24 22:25:25 +0200

    Improve interpreter tests

--- src/pkg/interpreter/interpreter_test.go
+++ src/pkg/interpreter/interpreter_test.go
@@ -70,6 +70,171 @@ var testCases = []struct {
 		expectedVariables: map[string]interface{}{"i": 4},
 		expectedOutput:    bytes.NewBufferString("0\n1\n2\n3\n4\n"),
 	},
+	{
+		name: "Plus operation works properly",
+		input: ast.Prog{
+			Statements: ast.Stmts{
+				Statements: []ast.Stmt{
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "x"),
+						VariableType: token.New(token.INTEGER, ""),
+						Expression:   ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+					},
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "s"),
+						VariableType: token.New(token.STRING, ""),
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "x")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+							Operator: token.New(token.PLUS, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+						},
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "s")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.StringOpnd{Value: "Hello "}},
+							Operator: token.New(token.PLUS, ""),
+							Right:    ast.NullaryExpr{Operand: ast.StringOpnd{Value: "World\n"}},
+						},
+					},
+				},
+			},
+		},
+		expectedVariables: map[string]interface{}{"x": 10, "s": "Hello World\n"},
+		expectedOutput:    bytes.NewBufferString(""),
+	},
+	{
+		name: "Minus operation works properly",
+		input: ast.Prog{
+			Statements: ast.Stmts{
+				Statements: []ast.Stmt{
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "x"),
+						VariableType: token.New(token.INTEGER, ""),
+						Expression:   ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "x")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+							Operator: token.New(token.MINUS, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+						},
+					},
+				},
+			},
+		},
+		expectedVariables: map[string]interface{}{"x": 0},
+		expectedOutput:    bytes.NewBufferString(""),
+	},
+	{
+		name: "Integer division works properly",
+		input: ast.Prog{
+			Statements: ast.Stmts{
+				Statements: []ast.Stmt{
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "x"),
+						VariableType: token.New(token.INTEGER, ""),
+						Expression:   ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "x")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+							Operator: token.New(token.INTEGER_DIV, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+						},
+					},
+				},
+			},
+		},
+		expectedVariables: map[string]interface{}{"x": 1},
+		expectedOutput:    bytes.NewBufferString(""),
+	},
+	{
+		name: "Multiplication works properly",
+		input: ast.Prog{
+			Statements: ast.Stmts{
+				Statements: []ast.Stmt{
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "x"),
+						VariableType: token.New(token.INTEGER, ""),
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "x")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+							Operator: token.New(token.MULTIPLY, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 5}},
+						},
+					},
+				},
+			},
+		},
+		expectedVariables: map[string]interface{}{"x": 25},
+		expectedOutput:    bytes.NewBufferString(""),
+	},
+	{
+		name: "Logical AND works properly",
+		input: ast.Prog{
+			Statements: ast.Stmts{
+				Statements: []ast.Stmt{
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "true"),
+						VariableType: token.New(token.BOOLEAN, ""),
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 1}},
+							Operator: token.New(token.EQ, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 1}},
+						},
+					},
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "false"),
+						VariableType: token.New(token.BOOLEAN, ""),
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 1}},
+							Operator: token.New(token.EQ, ""),
+							Right:    ast.NullaryExpr{Operand: ast.NumberOpnd{Value: 0}},
+						},
+					},
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "isTrue"),
+						VariableType: token.New(token.BOOLEAN, ""),
+					},
+					ast.DeclStmt{
+						Identifier:   token.New(token.IDENT, "isFalse"),
+						VariableType: token.New(token.BOOLEAN, ""),
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "isTrue")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.Ident{Id: token.New(token.IDENT, "true")}},
+							Operator: token.New(token.AND, ""),
+							Right:    ast.NullaryExpr{Operand: ast.Ident{Id: token.New(token.IDENT, "true")}},
+						},
+					},
+					ast.AssignStmt{
+						Identifier: ast.Ident{Id: token.New(token.IDENT, "isFalse")},
+						Expression: ast.BinaryExpr{
+							Left:     ast.NullaryExpr{Operand: ast.Ident{Id: token.New(token.IDENT, "true")}},
+							Operator: token.New(token.AND, ""),
+							Right:    ast.NullaryExpr{Operand: ast.Ident{Id: token.New(token.IDENT, "false")}},
+						},
+					},
+				},
+			},
+		},
+		expectedVariables: map[string]interface{}{
+			"true":    true,
+			"false":   false,
+			"isTrue":  true,
+			"isFalse": false,
+		},
+		expectedOutput: bytes.NewBufferString(""),
+	},
 }
 
 func TestInterpreter(t *testing.T) {
